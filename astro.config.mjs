84f359985c7b2ecd904c245fb2c04fd8333cf689
Matt Cotter <cotter.m@gmail.com>
2025-06-07 21:52:13 -0400

    update to try and fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mattcotter.dev',
   server: {
     host: true, 
   },
