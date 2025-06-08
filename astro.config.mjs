f94ed6c07d06244d4016bcf36909c7ef94ca3a82
Matt Cotter <cotter.m@gmail.com>
2025-06-08 14:49:49 -0400

    update to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mattcotter.dev',
+  site: 'https://www.mattcotter.dev',
   server: {
     host: true, 
   },
